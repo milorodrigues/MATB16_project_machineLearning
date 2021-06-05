960068171a2a085685dac07eb521933815508b05
Milo <milo.rodrigues97@gmail.com>
2021-06-05 19:02:37 -0300

    Initialized repo + preprocessing half done

---
+++ preprocessing.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import numpy as np
+
+# dataset source:
+# https://www.kaggle.com/kingabzpro/daylio-mood-tracker
+
+# Leitura do csv
+raw = pd.read_csv('Daylio_Abid.csv')
+# Remoção das colunas que não serão usadas
+raw.drop(['full_date', 'date', 'time', 'sub_mood'], axis=1, inplace=True)
+# Remoção das linhas com informação vazia
+raw.dropna(subset=['weekday', 'activities', 'mood'], inplace=True)
+
+# Determinação da lista de atividades possíveis + reformatação da coluna activities
+activities = set()
+for i in np.arange(raw.shape[0]):
+    a = raw.iloc[i]['activities'].split("|")
+    for j in np.arange(len(a)):
+        a[j] = a[j].strip()
+        activities.add(a[j])
+
+    newA = ""
+    for j in a:
+        newA += j + "|"
+    raw.iloc[i]['activities'] = newA[:-1]
+activities = list(activities)
+
+raw.to_csv('preprocessed_data.csv')
+
